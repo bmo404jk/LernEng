b8729742f6bede3d6225328d77de36ecf52e1045
bmo404jk <78249495+bmo404jk@users.noreply.github.com>
2025-01-11 16:18:37 +0400

    Add files via upload

--- js/burger_menu.js
+++ js/burger_menu.js
@@ -4,7 +4,11 @@ let burger_menu_content = document.querySelector(".burger_menu_content");
 burger_menu.addEventListener('click',function() {
     console.log(1);
     if(burger_menu_content.style.display == "none") {
+        document.documentElement.style.overflow = 'hidden';
          burger_menu_content.style.display = "flex";
+         burger_menu_content.style.position = "fixed";
+         burger_menu_content.style.zIndex = '100';
+        document.html
     } else {
         burger_menu_content.style.display = "none";
     }
