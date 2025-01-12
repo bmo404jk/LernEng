b2b8e681173f14f8196f48ae23b9c311c395d7a1
bmo404jk <78249495+bmo404jk@users.noreply.github.com>
2025-01-12 12:01:55 +0400

    Add files via upload

--- js/tests.js
+++ js/tests.js
@@ -4,6 +4,9 @@ let question_text = document.querySelector(".question_text1");
 let answer1 = document.querySelector(".answer1");
 let answer2 = document.querySelector(".answer2");
 let answer3 = document.querySelector(".answer3");
+let answer4 = document.querySelector(".answer4");
+
+let array_answer_elements = [answer1,answer2,answer3,answer4];
 
 let btn_next = document.querySelector(".button_next");
 
@@ -38,36 +41,131 @@ let array_questions = ["Did you ______ anything interesting last weekend?",
                     "We haven’t got _____ Indian relations.",
                     "Pam can’t _____ to you now. She is busy."];
 
+let array_answer1 = ["Do","Doing","Was","Did"];
+let array_answer2 = ["Is","Works","Does","Work"];
+let array_answer3 = ["He’s a(n)","His job is","His job is as","He work as a(n)"];
+let array_answer4 = ["Long","Far","Away","Many"];
+let array_answer5 = ["Will be able","Can","Won’t be able","Wouldn’t"];
+let array_answer6 = ["Have you been","Have you","Are you","Be you"];
+let array_answer7 = ["Has had","Has","Had had","Had"];
+let array_answer8 = ["Already","Never","Ever","Still"];
+let array_answer9 = ["For","Forward","Forward to","Of"];
+let array_answer10 = ["Was","Is","Had","Did"];
+let array_answer11 = ["Silence","Silent","Silently","Silenced"];
+let array_answer12 = ["Had known","Knew","Have known","Know"];
+let array_answer13 = ["Will","May","Might","Can"];
+let array_answer14 = ["Had just","Just","‘ve just","Soon will"];
+
+let array_answer15 = ["Away","Off","Up","In"];
+let array_answer16 = ["Shall I","Must I","Will I","Do I"];
+let array_answer17 = ["Nice","Nicely","Such nice","Like nice"];
+let array_answer18 = ["You coming","You come","You to come","That you come"];
+let array_answer19 = ["People adult enough","Adult people enough","Enough adult persons","Enough adult people"];
+let array_answer20 = ["Is made","Is making","Is make","Makes"];
+let array_answer21 = ["Left I my glasses","I left my glasses","Did I leave my glasses","The glasses I left"];
+let array_answer22 = ["Four times","Twice a","Every others","Every couple"];
+let array_answer23 = ["Plugged it in","Plug in it","Plug it in","Will plug it in"];
+let array_answer24 = ["A very lot","Lots","A lot of","Very much"];
+let array_answer25 = ["Has he got","Hasn’t he","Has he","Haven’t he"];
+let array_answer26 = ["Feel","Feel like","Like","Would like"];
+let array_answer27 = ["Won’t","Haven’t","Aren’t","Don’t"];
+let array_answer28 = ["Which","What","Whose","Who"];
+let array_answer29 = ["No","Any","None","Some"];
+let array_answer30 = ["To talk","Talked","Talk","Talking"];
+
+let array_answers = [array_answer1,
+    array_answer2,
+    array_answer3,
+    array_answer4,
+    array_answer5,
+    array_answer6,
+    array_answer7,
+    array_answer8,
+    array_answer9,
+    array_answer10,
+    array_answer11,
+    array_answer12,
+    array_answer13,
+    array_answer14,
+    array_answer15,
+    array_answer16,
+    array_answer17,
+    array_answer18,
+    array_answer19,
+    array_answer20,
+    array_answer21,
+    array_answer22,
+    array_answer23,
+    array_answer24,
+    array_answer25,
+    array_answer26,
+    array_answer27,
+    array_answer28,
+    array_answer29,
+    array_answer30
+];
+
 let count_of_number_test = 1;
+let x = 1;
 let i = 1;
+
 answer1.addEventListener("click",function(){
-    console.log("answer1");
+    if(answer1.textContent == array_answer1[0]) {
+        console.log("Правильный ответ");
+    }else {
+        console.log("НЕ правильный ответ");
+        console.log(answer1.textContent);
+    }
 })
 answer2.addEventListener("click",function(){
-    console.log("answer2");
+    if(answer2.textContent == array_answer1[0]) {
+        console.log("Правильный ответ");
+    }else {
+        console.log("НЕ правильный ответ");
+        console.log(answer1.textContent);
+    }
 })
 answer3.addEventListener("click",function(){
-    console.log("answer3");
+    if(answer3.textContent == array_answer1[0]) {
+        console.log("Правильный ответ");
+    }else {
+        console.log("НЕ правильный ответ");
+        console.log(answer1.textContent);
+    }
+})
+answer4.addEventListener("click",function(){
+    if(answer4.textContent == array_answer1[0]) {
+        console.log("Правильный ответ");
+    }else {
+        console.log("НЕ правильный ответ");
+        console.log(answer1.textContent);
+    }
 })
 
 btn_next.addEventListener('click',function(){
-    if(i > array_questions.length) {
-        i = 1;
+    if(i >= array_questions.length) {
+        i = 0;
+        x = 0;
         count_of_number_test = 0;
     }
-    console.log(test_number.innerHTML);
-console.log("Did you ______ anything interesting last weekend?");
 
-if (test_number.innerHTML.trim().toLowerCase() === "did you ______ anything interesting last weekend?".trim().toLowerCase()) {
-    console.log("true");
-    test_number.innerHTML = "2";
-    question_text.innerHTML = array_questions[1];
-} else {
-    console.log("false");
-}
-    test_number.innerHTML = count_of_number_test++;
+    test_number.innerHTML = count_of_number_test+=1;
     question_text.innerHTML = array_questions[i];
     i++;
-    console.log("I:" + i);
-    console.log("count_of_number_test:" + count_of_number_test);
+
+    function shuffleArray(array) {
+        for (let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+    }
+
+    shuffleArray(array_answers[x]);
+
+    let array = array_answers[x];
+    for(let i = 0; i < 4; i++) {
+        console.log(array);
+        array_answer_elements[i].innerHTML = array[i];
+    }
+    x++;    
 });
