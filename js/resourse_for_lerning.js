ec87e9d83c204ebd3735fcab6099653ffde9ae95
bmo404jk <78249495+bmo404jk@users.noreply.github.com>
2025-01-15 12:58:08 +0400

    Add files via upload

--- js/resourse_for_lerning.js
+++ js/resourse_for_lerning.js
@@ -1,7 +1,22 @@
-let h3_titles = document.querySelectorAll(".h3_title");
-h3_titles.forEach((h3_title, index) => {
-    let card_container = h3_title.nextElementSibling; 
-    h3_title.addEventListener("click", () => {
+let h3_title_1 = document.querySelectorAll(".h3_title_1");
+let h3_title_2 = document.querySelectorAll(".h3_title_2");
+let h3_title_3 = document.querySelectorAll(".h3_title_3");
+
+h3_title_1.forEach((h3_title_1, index) => {
+    let card_container = h3_title_1.nextElementSibling; 
+    h3_title_1.addEventListener("click", () => {
+        card_container.classList.toggle('expanded'); 
+    });
+});
+h3_title_2.forEach((h3_title_2, index) => {
+    let card_container = h3_title_2.nextElementSibling; 
+    h3_title_2.addEventListener("click", () => {
+        card_container.classList.toggle('expanded'); 
+    });
+});
+h3_title_3.forEach((h3_title_3, index) => {
+    let card_container = h3_title_3.nextElementSibling; 
+    h3_title_3.addEventListener("click", () => {
         card_container.classList.toggle('expanded'); 
     });
 });
