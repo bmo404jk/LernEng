ab37d9c0d812d99d76016bb0c2581be5c3b0ef3c
bmo404jk <78249495+bmo404jk@users.noreply.github.com>
2025-03-31 11:33:20 +0400

    Add files via upload

--- js/burger_menu.js
+++ js/burger_menu.js
@@ -1,26 +1,13 @@
 let burger_menu = document.querySelector(".burger_menu");
 let burger_menu_content = document.querySelector(".burger_menu_content");
-let nav_mobile = document.querySelector(".nav_mobile");
-
 
 burger_menu.addEventListener('click', function() {
     burger_menu_content.classList.toggle('expanded');
 
-    const contentBlocks = document.querySelectorAll('.benefit_block');
     if (burger_menu_content.classList.contains('expanded')) {
-        // Скрываем элементы
-        contentBlocks.forEach(block => {
-            block.style.display = "none"; // Скрываем блоки при открытом меню
-        });
-        nav_mobile.style.position = "relative"; // Позиционирование навигации
-       // document.documentElement.style.overflow = 'hidden'; // Блокируем прокрутку
+        document.body.style.overflow = 'hidden'; // Блокируем прокрутку
     } else {
-        // Показываем элементы
-        contentBlocks.forEach(block => {
-            block.style.display = "flex"; // Показываем блоки при закрытом меню
-        });
-        nav_mobile.style.position = "fixed"; // Устанавливаем фиксированное позиционирование
-        document.documentElement.style.overflow = 'inherit'; // Восстанавливаем прокрутку
+        document.body.style.overflow = 'inherit'; // Восстанавливаем прокрутку
     }
 });
 
